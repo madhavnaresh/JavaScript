c2148d3858d82647a18200d469043cff319b7983
Madhav Gupta <madhavgupta639@gmail.com>
2024-06-27 13:32:39 +0530

    Add files via upload

---
+++ c1.js
@@ -0,0 +1,52 @@
+// console.log("Hello world","Madhav",21,"MCA");
+
+// let a=10;
+// var b=20;
+// const c=30;
+// console.log(a,b,c); 
+
+// Primitive data types
+/*
+let d=1;
+let e="Madhav";
+let f=true;
+let g=undefined;
+let h=null;
+let i=BigInt(4729729429478284);
+let j=Symbol("heyy");
+
+console.log(d,e,f,g,h,i,j);
+console.log(typeof d, typeof e, typeof f, typeof g, typeof h, typeof i, typeof j); 
+*/
+
+//Non-pritive data type
+
+/*
+let person={
+    fname:'Madhav',
+    lname:'Gupta',
+    age:21,
+    isMarried:false
+}
+person.age=22; // to update values of item in object
+person.college="Tula's Institute"; // to add item in object
+console.log(person);
+console.log(person.age); // to print specific value of object
+*/
+
+//implicit type conversion :- Type coercion
+/*
+console.log(35+"mg");
+console.log(35+"23");
+console.log(typeof (35+"mg"));
+*/
+
+
+//explicit type conversion:- Type casting
+
+let a=10;
+let s=String(a);
+console.log(typeof a);
+console.log(typeof s);
+
+//Chapter 1 completed
