686ec91ed48589162bdb963b7239ab5f245f56a5
madhavnaresh <madhavgupta639@gmail.com>
2024-06-28 11:18:07 +0530

    edit chapter 2

--- chap2.js
+++ chap2.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+// if else
+let grocerySpending=300; 
+if (grocerySpending > 1000)
+    {
+        let discount=0.10 * grocerySpending;
+        console.log("You get 10% discount ", discount);
+    }
+else if(grocerySpending > 500)
+    {
+        let discount=0.05 * grocerySpending;
+        console.log("You get 5% discount", discount);
+    }
+else
+    {
+        console.log("No discount");
+    }
