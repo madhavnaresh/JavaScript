96309f08b4f6f4b7a2b84e71e333736fb4f08600
madhavnaresh <madhavgupta639@gmail.com>
2024-07-07 20:31:23 +0530

    define function

--- chap6.js
+++ chap6.js
@@ -1,1 +1,10 @@
 //Function and Execution
+
+function addtocart(product)
+    {
+        console.log(`${product} is added to cart.`);
+    }
+
+addtocart("Pen");
+addtocart("Tablet");
+addtocart("Shoes");
