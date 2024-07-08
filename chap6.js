f18614be69e77a180f1396af447faced06592a2c
madhavnaresh <madhavgupta639@gmail.com>
2024-07-08 23:53:31 +0530

    add functio typen

--- chap6.js
+++ chap6.js
@@ -13,8 +13,40 @@ addtocart("Shoes");
 */
 
 //Anonymous function
+/*
 let sum=function()
     {
         console.log(2+9);
     }
-sum();
+sum(); //calling the function
+*/
+
+//Higher order function
+/*
+//Passing function as argument
+function getclicked(clicked)
+    {
+        clicked();
+    }
+
+getclicked(function() //function calling
+    {
+        console.log("i am clicked");
+    }    
+)
+
+//returning a function as a value from that
+function returnFunction()
+{
+    return function()
+        {
+            console.log("returning");
+        }
+}
+//calling
+//1
+returnFunction()(); 
+//2
+let rf=returnFunction();
+rf();
+*/
