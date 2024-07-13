77cbccfb8029c3deee26371c3d14a28aff1d58c1
madhavnaresh <madhavgupta639@gmail.com>
2024-07-13 22:29:04 +0530

    add content

--- chap7.js
+++ chap7.js
@@ -1,4 +1,4 @@
-//For each loop
+//ForEach() loop
 // let count = [3, 4, 1];
 // count.forEach((number, index, count) => // number=individual value of array, index=index number of array, count=whole array
 // {
@@ -8,14 +8,15 @@
 
 //Another example
 
-let product=[
-    {id:1,Name:"prod 1", price:99},
-    {id:2,Name:"prod 2", price:199},
-    {id:3,Name:"prod 3", price:299},
-    {id:4,Name:"prod 4", price:399},
-    {id:5,Name:"prod 5", price:499},
+let product = [
+    { id: 1, Name: "prod 1", price: 99 },
+    { id: 2, Name: "prod 2", price: 199 },
+    { id: 3, Name: "prod 3", price: 149 },
+    { id: 4, Name: "prod 4", price: 399 },
+    { id: 5, Name: "prod 5", price: 49 },
 ]
 
+/*
 product.forEach((product)=>
     {
         console.log(`Product Id:- ${product.id}`);
@@ -23,8 +24,20 @@ product.forEach((product)=>
         console.log(`Product Price:- ${product.price}\n`);
     }
 )
+
+//Map() method
 let maparray=product.map((product)=> //it is all like the foreach() but it creates an array and stores all values in that whatever the condition is it stores in new array
     {
         console.log(`2X Price of Product ${product.id}:- ${product.price *2}`);}
 )
+*/
 
+//Filter() method 
+let filteredProd=product.filter((product)=> // it also creates a separate array of values according to the condition
+    {
+        return product.price > 150;
+    }
+)
+
+console.log(filteredProd);
+console.log(product);
